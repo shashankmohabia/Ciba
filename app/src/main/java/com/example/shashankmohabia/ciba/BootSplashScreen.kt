08d520484fa74352b7705cb5ca5b44485bc3429c
killbotxd <sonawane.1@iitj.ac.in>
2018-12-08 21:18:21 +0530

    second commit for boot splash screen

--- app/src/main/java/com/example/shashankmohabia/ciba/BootSplashScreen.kt
+++ app/src/main/java/com/example/shashankmohabia/ciba/BootSplashScreen.kt
@@ -1,2 +1,27 @@
 package com.example.shashankmohabia.ciba
 
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import java.lang.Thread.sleep
+
+class BootSplashScreen : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstancestate: Bundle?){
+        super.onCreate(savedInstancestate)
+        setContentView(R.layout.bootsplashscreen)
+
+        val intent= Intent(this,MainActivity::class.java)
+       Thread{
+         try{
+            sleep(1500)
+            }finally{
+                            startActivity(intent)
+                             finish()
+                    }
+       }.start()
+
+
+    }
+}
