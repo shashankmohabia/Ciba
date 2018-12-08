722f77b8efbcd6493922eeda0319a51afbf158c6
killbotxd <sonawane.1@iitj.ac.in>
2018-12-09 02:54:42 +0530

    login scrreen commit

---
+++ app/src/main/java/com/example/shashankmohabia/ciba/Login_customer.kt
@@ -0,0 +1,14 @@
+package com.example.shashankmohabia.ciba
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Login_customer: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_customer)
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/shashankmohabia/ciba/Login_merchant.kt
@@ -0,0 +1,16 @@
+package com.example.shashankmohabia.ciba
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Login_merchant:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_merchant)
+
+    }
+
+
+
+
+}
