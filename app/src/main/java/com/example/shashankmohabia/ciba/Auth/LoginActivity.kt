b0a2c83a4972302ecf8a7f0ab27b15e35e349722
killbotxd <sonawane.1@iitj.ac.in>
2018-12-11 13:58:17 +0530

    added dark shadow to merchant

--- app/src/main/java/com/example/shashankmohabia/ciba/Auth/LoginActivity.kt
+++ app/src/main/java/com/example/shashankmohabia/ciba/Auth/LoginActivity.kt
@@ -2,10 +2,14 @@ package com.example.shashankmohabia.ciba.Auth
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 
+val auth=FirebaseAuth.getInstance()
 class LoginActivity:AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 }
