a5284d0b00a285b07eebe759f12174d0f20a5b46
killbotXD <42870378+killbotXD@users.noreply.github.com>
2018-12-15 22:38:26 +0530

    add Splash Activity | add  Information Activity | add  User Type Activity
    * initial commit for boot splash screen
    
    * second commit for boot splash screen
    
    * second commit for boot splash screen
    
    * commit for info screen color values
    
    * info screen and choosing activity
    
    * login scrreen commit
    
    * login scrreen commit
    
    * sign in with email and google added
    
    * Everything working splash+info+utype

---
+++ app/src/main/java/com/example/shashankmohabia/ciba/UI/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.shashankmohabia.ciba.UI
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import com.example.shashankmohabia.ciba.R
+
+class SplashScreen : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstancestate: Bundle?){
+        super.onCreate(savedInstancestate)
+        setContentView(R.layout.splashscreen)
+        val lo:ImageView=findViewById<ImageView>(R.id.logo)
+        lo.setOnClickListener {
+        val intent= Intent(this, Info::class.java)
+
+        startActivity(intent)
+        finish()}
+
+    }
+}
---
+++ app/src/main/java/com/example/shashankmohabia/ciba/UserType/UserTypeSelectionActivity.kt
@@ -0,0 +1,13 @@
+package com.example.shashankmohabia.ciba.UserType
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.shashankmohabia.ciba.R
+
+class UserTypeSelectionActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_type_selection)
+
+    }
+}
