595f45c3b40b90481334ec4b65072cee31666e6a
killbotxd <sonawane.1@iitj.ac.in>
2019-03-19 20:18:16 +0530

    created fragment for popup

--- app/src/main/java/com/example/shashankmohabia/ciba/Fragments/EnterDetailsForFirstTime.kt
+++ app/src/main/java/com/example/shashankmohabia/ciba/Fragments/EnterDetailsForFirstTime.kt
@@ -1,2 +1,19 @@
 package com.example.shashankmohabia.ciba.Fragments
 
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shashankmohabia.ciba.R
+
+class EnterDetailsForFirstTime: Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.getinfo,container,false)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
